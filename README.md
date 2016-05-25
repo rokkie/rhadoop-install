fad7ced1a3f019597ebf8ff568ec7d5a63d6995b
Rocco Bruyn <rocco.bruyn@gmail.com>
2016-05-25 09:17:38 +0200

    Add scripts
    First version, untested.

--- README.md
+++ README.md
@@ -4,4 +4,14 @@ Automated script to install RHadoop on Hortonworks Sandbox.
 Tested with version 2.1 found at:
 (http://hortonworks.com/downloads/#sandbox)
 
+## Instructions
+1. Download Hortonworks VM 2.1
+2. Import the image
+3. Mount/share a folder from the host machine
+4. Copy install files to the shared folder
+5. Boot VM
+6. Login into VM as root via SSH
+7. Copy files from shared folder to home directory
+8. Run: `./install-rhadoop-deps.sh`
+9. Reboot VM
 
---
+++ install-rhadoop.R
@@ -0,0 +1,49 @@
+#!/usr/bin/R
+
+# download using wget
+print("use wget for downloading")
+options(download.file.method = "wget")
+
+# install rmr2
+print("installing dependencies for rmr2")
+install.packages("Rcpp")
+install.packages("RJSONIO")
+install.packages("digest")
+install.packages("functional")
+install.packages("reshape2")
+install.packages("stringr")
+install.packages("plyr")
+install.packages("caTools")
+print("installing rmr2")
+install.packages("~/rmr2_3.3.1.tar.gz", repos = NULL, type = "source")
+
+# install plyrmr
+print("installing dependencies for plymer")
+install.packages("dplyr")
+install.packages("R.methodsS3")
+install.packages("Hmisc")
+install.packages("memoise")
+install.packages("lazyeval")
+install.packages("rjson")
+print("installing plymer")
+install.packages("~/plyrmr_0.6.0.tar.gz", repos = NULL, type = "source")
+
+# install ravro
+print("installing dependencies for ravro")
+install.packages("bit64")
+print("installing ravro")
+install.packages("~/ravro_1.0.4.tar.gz", repos = NULL, type = "source")
+
+# install rhdfs
+print("installing dependencies for rhdfs")
+install.packages("rJava")
+print("installing rhdfs")
+install.packages("~/rhdfs_1.0.8.tar.gz", repos = NULL, type = "source")
+
+# install rhbase
+print("installing hbase")
+install.packages("~/rhbase_1.2.1.tar.gz", repos = NULL, type = "source")
+
+# done
+print("the horrorshow is over, you can now do RHadoop")
+
